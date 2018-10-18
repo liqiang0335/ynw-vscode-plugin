6a0d8d4dc80e88ad99207a91080e3b381d175d29
liqiang <liqiang0335@qq.com>
2018-10-18 14:24:05 +0800

    update

--- src/handler/WebpackBuild.js
+++ src/handler/WebpackBuild.js
@@ -9,7 +9,7 @@ const exec = cmd => {
 
 module.exports = function(URI, env) {
   const relative = vscode.workspace.asRelativePath(URI);
-  const dirname = path.dirname(relative);
+  const dirname = path.dirname(relative).match(/\w+$/);
   const entry = relative.replace(/\\+/g, "/").replace(/\.[a-z]+$/, "");
-  exec(`ynw build=${dirname} entry=${entry} env=${env}`);
+  exec(`ynw build=${dirname[0]} entry=${entry} env=${env}`);
 };
