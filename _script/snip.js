636d81a5c38d23eb3364cd70169a096dd1752857
liqiang <liqiang0335@qq.com>
2019-08-09 10:23:09 +0800

    add ynw to vue

--- _script/snip.js
+++ _script/snip.js
@@ -1,18 +1,20 @@
 const fs = require("fs");
 const path = require("path");
 const getFiles = require("./utils/getFiles");
-const target = path.join(__dirname, "../snippets/ynw.json");
+const ynw = path.join(__dirname, "../snippets/ynw.json");
 
 // Files
 const SOURCE = "D:\\Git\\ynw";
 
 main();
 function main() {
-  const files = getFiles()(SOURCE).filter(it => {
-    return it.extname === ".js" && !/vue/.test(it.filePath);
-  });
-  const content = files.map(item => template(item)).join(",");
-  fs.writeFileSync(target, `{${content}}`);
+  const files = getFiles()(SOURCE);
+  const jsfiles = files.filter(it => it.extname === ".js");
+
+  const content = jsfiles.map(item => template(item)).join(",");
+  fs.writeFileSync(ynw, `{${content}}`);
+
+  // add ynw to vue.json
 }
 
 function template(item) {
