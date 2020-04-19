875483175af8076f4ac592640a67718fc1c10fb2
liqiang0335@qq.com <liqiang0335@gmail.com>
2020-04-19 15:04:51 +0800

    add

--- src/utils/getLocalFile.js
+++ src/utils/getLocalFile.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const vscode = require("vscode");
 const { workspace } = vscode;
-
+/**
+ * 获取工作区根目录文件
+ */
 module.exports = name => {
   return path
     .join(workspace.workspaceFolders[0].uri.path, name)
