35430842965d27965463208f375a6d633435e49a
liqiang0335 <liqiang0335@qq.com>
2020-10-06 17:39:44 +0800

    add

--- src/handler/fastInput.js
+++ src/handler/fastInput.js
@@ -6,16 +6,14 @@ const getLocalFastValue = () => {
   const CopyPath = getLocalFile("ynw-copy.txt");
   const FastPath = getLocalFile("ynw-fast.js");
 
-  // copy
   if (fs.existsSync(CopyPath)) {
     return fs.readFileSync(CopyPath, "utf-8");
   }
-  // Dynamic content
   if (fs.existsSync(FastPath)) {
     const value = require(FastPath)();
     return `${value}`;
   }
-  // Default
+
   return `${Date.now()}`;
 };
 
