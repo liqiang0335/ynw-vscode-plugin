1ca82fac2e7d243056b861f095479bedc3d09181
liqiang0335@qq.com <liqiang0335@gmail.com>
2020-11-14 16:48:31 +0800

    add

--- src/handler/CopyName.js
+++ src/handler/CopyName.js
@@ -1,13 +1,17 @@
 const vscode = require("vscode");
+
 /**
  * ----------------------------------------
  *   导入公共模块时, 设置相同的名称
  * ----------------------------------------
  */
-module.exports = function (URI) {
+module.exports = function () {
   const editor = vscode.window.activeTextEditor;
   const document = editor.document;
   const selecton = editor.selection.end;
   const line = document.lineAt(selecton);
-  console.log(line);
+  const lineText = document.getText(line);
+  const match = lineText.match(/\/(\w+)/);
+  const target = match ? match[1] : lineText;
+  console.log(target);
 };
