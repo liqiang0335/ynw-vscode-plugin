6851d8a8ab7aa1bff7485e6cbc9eafadd15ce2a9
liqiang0335 <liqiang0335@qq.com>
2022-07-13 21:04:14 +0800

    add

--- src/handler/CreateSameNameFile.js
+++ src/handler/CreateSameNameFile.js
@@ -5,9 +5,7 @@ const { copy } = require("copy-paste");
 const getConfigValue = require("../utils/getConfigValue");
 
 module.exports = async function (URI) {
-  const filePath = URI
-    ? URI.fsPath
-    : vscode.window.activeTextEditor.document.fileName;
+  const filePath = URI ? URI.fsPath : vscode.window.activeTextEditor.document.fileName;
 
   const stat = await utils.stat(filePath);
   const fileType = stat.isDirectory() ? "dir" : "file";
@@ -28,10 +26,9 @@ module.exports = async function (URI) {
       const name = filePath.match(/\w+$/)[0];
       const target = path.join(filePath, name + "." + scriptFileType);
       const content = `import React from "react";\n\n
-      function ${name}(){
+      export default function ${name}(){
         return <div>${name}</div>
-      }
-      export default ${name}`;
+      }`;
       return { target, content };
     },
   };
