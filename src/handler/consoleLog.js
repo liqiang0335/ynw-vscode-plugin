7cb9d1397530aa4dea9b845a0e5726bb3158b9ad
liqiang0335 <liqiang0335@qq.com>
2023-12-15 18:33:37 +0800

    Fixed

--- src/handler/consoleLog.js
+++ src/handler/consoleLog.js
@@ -1,5 +1,6 @@
 const vscode = require("vscode");
 const replaceText = require("../utils/replaceText");
+const dayjs = require("dayjs");
 /**
  * ----------------------------------------
  * 打印选中变量
@@ -14,13 +15,17 @@ module.exports = function () {
 
   const selection = editor.selection;
   const text = editor.document.getText(selection);
+  const time = dayjs().format("YYYY-MM-DD HH:mm:ss");
+
+  // 当前文件名
+  const currentFileName = editor.document.fileName.split("/").pop();
 
   if (text) {
     vscode.commands.executeCommand("editor.action.insertLineAfter").then(() => {
-      const value = `console.log("⭕️ ${text}: ", ${text});`;
+      const value = `console.log("⭕️ [${currentFileName}] [${time}] ${text}: ", ${text});`;
       replaceText(value);
     });
   } else {
-    replaceText(`console.log("⭕️","");`);
+    replaceText(`console.log("⭕️ [${currentFileName}] [${time}]","");`);
   }
 };
